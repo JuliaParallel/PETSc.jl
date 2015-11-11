ba4c892b42005270eaa312d08b487103555fd7bf
Steven G. Johnson <stevenj@mit.edu>
2015-11-11 14:53:07 -0500

    pure-Julia build script

--- deps/build_petscs.jl
+++ deps/build_petscs.jl
@@ -7,91 +7,70 @@ fmt = ".tar.gz"
 file_name = string(petsc_name, fmt)
 
 if !isfile(file_name)
-  println("Downloading $file_name")
+  println("DOWNLOADING $file_name")
   download("http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/$file_name", file_name)
 end
 
+const verbose=false # change to true for verbose compilation output
 
-start_dir = dirname(@__FILE__)
-if true  # debugging flag
-
-# delete existing installation
-name_i = "RealDouble"
-if isdir(name_i)
-  println("deleting existing installation $name_i")
-  rm("$name_i", recursive=true)
+# the PETSc configure script does not support Python 3, so we need to
+# find Python 2 if we can:
+pyconfigvar(python::AbstractString, var::AbstractString) = chomp(readall(`$python -c "import distutils.sysconfig; print(distutils.sysconfig.get_config_var('$var'))"`))
+pythonvers(python) = VersionNumber(pyconfigvar(python, "VERSION"))
+pythonok(python) = try
+  return pythonvers(python) < v"3"
+catch
+  return false
 end
-# create new one
-mkdir(name_i)
-cd(name_i)
-println("extracting PETSc $name_i")
-run(`tar xfz ../$file_name`)
-println("finished extracting PETSc $name_i")
-cd(petsc_name)
-run(`$start_dir/install_petsc.sh --with-64-bit-indices=true --with-scalar-type=real --with-precision=double`)
+const PYTHON = get(ENV, "PYTHON", "python")
+const python = pythonok(PYTHON) ? PYTHON : pythonok("python") ? "python" :
+  pythonok("python2") ? "python2" : pythonok("python2.7") ? "python2.7" :
+  error("could not find Python version < 3 for PETSc configure script")
 
-cd(start_dir)
+arches = Dict() # dict of dirname => PETSC_ARCH
 
-# delete existing installation
-name_i = "RealSingle"
-if isdir(name_i)
-  println("deleting existing installation $name_i")
-  rm("$name_i", recursive=true)
-end
-# create new one
-mkdir(name_i)
-cd(name_i)
-println("extracting PETSc $name_i")
-run(`tar xfz ../$file_name`)
-println("finished extractin PETSc $name_i")
-cd(petsc_name)
-run(`$start_dir/install_petsc.sh --with-64-bit-indices=true --with-scalar-type=real --with-precision=single`)
-
-
-cd(start_dir)
-
-# delete existing installation
-name_i = "ComplexDouble"
-if isdir(name_i)
-  println("deleting existing installation $name_i")
-  rm("$name_i", recursive=true)
-end
-# create new one
-mkdir(name_i)
-cd(name_i)
-println("extracting PETSc $name_i")
-run(`tar xfz ../$file_name`)
-println("finished extracting PETSc $name_i")
-cd(petsc_name)
-run(`$start_dir/install_petsc.sh --with-64-bit-indices=true --with-scalar-type=complex --with-precision=double`)
-
-
-cd(start_dir)
-
-end
-# create library locations
-
-  # get PETSC_ARCH
-  f = open("./RealDouble/petsc_evars")
-  evars = readlines(f)
-  close(f)
-  petsc_arch = evars[2][1:(end-1)]
-  petscRealDouble = joinpath(pwd(), "RealDouble/$petsc_name/$petsc_arch/lib/libpetsc")
-  petscRealSingle = joinpath(pwd(), "RealSingle/$petsc_name/$petsc_arch/lib/libpetsc")
-  petscComplexDouble = joinpath(pwd(), "ComplexDouble/$petsc_name/$petsc_arch/lib/libpetsc")
-
-
-
-
-  f = open("lib_locations.jl", "w")
-  write(f, "global const petscRealDouble = \"$petscRealDouble\"\n")
-  write(f, "global const petscRealSingle = \"$petscRealSingle\"\n")
-  write(f, "global const petscComplexDouble = \"$petscComplexDouble\"\n")
-  close(f)
-
-
-  if isfile("../src/generated/lib_locations.jl")
-    rm("../src/generated/lib_locations.jl")
+for scalar_type in ("real", "complex"), precision in ("double", "single")
+  # extract the tarfile into a new directory:
+  name_i = ucfirst(scalar_type) * ucfirst(precision)
+  name_i == "ComplexSingle" && continue # PETSc doesn't support this (weird!)
+  println("****************************************************************")
+  if isdir(name_i)
+    verbose && println("deleting existing installation $name_i")
+    rm("$name_i", recursive=true)
   end
+  mkdir(name_i)
+  println("EXTRACTING PETSc to $name_i...")
+  run(`tar xz -C $name_i -f $file_name`)
 
-  cp("lib_locations.jl", "../src/generated/lib_locations.jl")
+  cd(joinpath(name_i, petsc_name)) do
+    withenv("PETSC_DIR"=>nothing, "PETSC_ARCH"=>nothing) do
+      println("CONFIGURING PETSc $name_i...")
+      PETSC_ARCH=""
+      for line in eachline(`$python configure --with-64-bit-indices=true --with-scalar-type=$scalar_type --with-precision=$precision`)
+        verbose && print("    $line")
+        if ismatch(r"^\s*PETSC_ARCH:", line)
+          PETSC_ARCH = chomp(split(line)[2])
+        end
+      end
+      isempty(PETSC_ARCH) && error("couldn't determine PETSC_ARCH")
+      arches[name_i] = PETSC_ARCH
+      println("BUILDING PETSc $name_i...")
+      for line in eachline(`make MAKE_NP=$CPU_CORES PETSC_DIR=$(pwd()) PETSC_ARCH=$PETSC_ARCH`)
+        verbose && print("    $line")
+      end
+      println("TESTING PETSc $name_i...")
+      for line in eachline(`make MAKE_NP=$CPU_CORES PETSC_DIR=$(pwd()) PETSC_ARCH=$PETSC_ARCH test`)
+        verbose && print("    $line")
+      end
+    end
+  end
+end
+
+# create deps.jl file with library locations
+open("deps.jl", "w") do f
+  for name in ("RealDouble", "RealSingle", "ComplexDouble")
+    path = abspath(name, petsc_name, arches[name], "lib", "libpetsc")
+    println(f, "const petsc$name = \"", escape_string(path), "\"")
+  end
+  println(f, "const PetscInt = Int64")
+end

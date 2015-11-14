64eb4bc8367f6714c63b1bcce0c7b2f641419b23
Jared Crean <jcrean01@gmail.com>
2015-11-14 00:10:42 -0500

    fixed MPI comm bug for index sets

--- src/generated/defs2.jl
+++ src/generated/defs2.jl
@@ -3,7 +3,7 @@ typealias PetscCopyMode Int32
 PETSC_COPY_VALUES = Int32(0)
 
 function ISCreateGeneral(arg1::MPI_Comm,arg2::Integer,arg3::Union{Ptr{Int64},StridedArray{Int64},Ptr{Int64},Ref{Int64}}, arg3a::Integer,arg4::Union{Ptr{IS{Float64}},StridedArray{IS{Float64}},Ptr{IS{Float64}},Ref{IS{Float64}}})
-    err = ccall((:ISCreateGeneral,petscRealDouble),PetscErrorCode,(comm_type,Int64,Ptr{Int64},Cint,Ptr{IS{Float64}}),arg1.val,arg2,arg3,arg3a,arg4)
+    err = ccall((:ISCreateGeneral,petscRealDouble),PetscErrorCode,(comm_type,Int64,Ptr{Int64},Cint,Ptr{IS{Float64}}),arg1,arg2,arg3,arg3a,arg4)
     return err
 end
 

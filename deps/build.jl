485cdb75bef9256fe274d709fb3ca8d1c09bd909
Jared Crean <jcrean01@gmail.com>
2016-04-19 23:03:08 -0400

    install MPI.jl through REQUIRE file

--- deps/build.jl
+++ deps/build.jl
@@ -4,11 +4,4 @@ if isfile("deps.jl")
   rm("deps.jl")
 end
 
-pkg_dict = Pkg.installed()  # get dictionary of installed package names to version numbers
-
-if !haskey(pkg_dict, "MPI")
-  Pkg.clone("MPI")
-  Pkg.build("MPI")
-end
-
 include("build_petscs.jl")

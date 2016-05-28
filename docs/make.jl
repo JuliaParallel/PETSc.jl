0b716658631a32865c43f7e68fd1208f40b6b9be
Jared Crean <jcrean01@gmail.com>
2016-05-27 20:10:29 -0400

    update travis to generate docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,9 @@
 using Documenter, PETSc
 
 makedocs(modules = [PETSc])
+
+deploydocs(
+  deps = Deps.pip("mkdocs", "python-markdown-math"),
+  repo = "github.com/JuliaParallel/PETSc.jl.git",
+  julia = "0.4",
+)

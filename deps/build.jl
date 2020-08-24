ad07994443a3c527510b4ad6d037b65c9f8457fb
Simon Byrne <simonbyrne@gmail.com>
2020-08-24 09:18:30 -0700

    delete deps dir, travis script

--- deps/build.jl
+++
@@ -1,9 +0,0 @@
-# deps.jl is created at the end of a successful build, so rm
-# to ensure that failed builds are missing this file.
-if isfile("deps.jl")
-  rm("deps.jl")
-end
-
-#Pkg.checkout("MPI", "master")
-#Pkg.build("MPI")
-include("build_petscs.jl")

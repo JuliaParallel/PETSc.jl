0f4db17e2684c6fff82dee86be5577d358afb118
Boris Kaus <kaus@uni-mainz.de>
2021-05-17 09:01:38 +0200

    signficant speedup by utilizing the sparsity pattern of the jacobian

--- examples/DMSTAG_porwave_1D.jl
+++ examples/DMSTAG_porwave_1D.jl
@@ -9,7 +9,7 @@
 
 using PETSc, MPI
 using Plots
-using ForwardDiff, SparseArrays
+using SparseArrays, SparseDiffTools, ForwardDiff
 
 PETSc.initialize()
 
@@ -53,6 +53,18 @@ function  ForwardDiff_res(x, user_ctx)
     return f;
 end
 
+function  f(out, x, user_ctx)
+
+    ArrayLocal_x     =   PETSc.DMStagVecGetArray(user_ctx.dm, x);        # array with all local x-data
+    ArrayLocal_f     =   PETSc.DMStagVecGetArray(user_ctx.dm, out);        # array with all local residual
+    
+    ComputeLocalResidual(user_ctx.dm, ArrayLocal_x, ArrayLocal_f, user_ctx);
+
+    return nothing
+end
+
+
+
 function FormJacobian!(cx_g, J, P, user_ctx)
     # This requires several steps:
     #
@@ -64,15 +76,35 @@ function FormJacobian!(cx_g, J, P, user_ctx)
     PETSc.DMGlobalToLocal(user_ctx.dm, cx_g,  PETSc.INSERT_VALUES,  user_ctx.x_l) 
     x               =   PETSc.unsafe_localarray(Float64, user_ctx.x_l.ptr;  write=false, read=true)
 
-    f_Residual      =   (x -> ForwardDiff_res(x, user_ctx));        # pass additional arguments into the routine
 
-    J_julia         =   ForwardDiff.jacobian(f_Residual,x);  
+    if isnothing(user_ctx.jac)
+        # Compute sparsity pattern of jacobian. This is relatvely slow, but only has to be done once.
+        # Theoretically, more efficient tools for this exists (jacobian_sparsity in the SparsityDetection.jl package),
+        # but they don't seem to work with the PETSc approach we use. Therefore we employ      
+        f_Residual  =   (x -> ForwardDiff_res(x, user_ctx));        # pass additional arguments into the routine
+        J_julia     =   ForwardDiff.jacobian(f_Residual,x);  
 
-    # @show J_julia, size(J_julia)
-    n                =   size(P,1)
-    J               .=   sparse(J_julia[1:n,1:n]);       
+        # employ sparse structure to compute jacobian - to be moved inside routines
+        jac         =   sparse(J_julia);
+        colors      =   matrix_colors(jac)          # the number of nonzeros per row
 
-   return J_julia
+    else
+        jac     =   user_ctx.jac;
+        colors  =   user_ctx.colors;
+
+    end
+    out         =   similar(x);
+        
+   f_Res           =   ((out,x)->f(out, x, user_ctx));        # pass additional arguments into the routine
+   forwarddiff_color_jacobian!(jac, f_Res, x, colorvec = colors)
+
+    ind             =   PETSc.LocalInGlobalIndices(user_ctx.dm);    # extr
+    J              .=   jac[ind,ind];    
+
+    user_ctx.jac    =   jac;
+    user_ctx.colors =   colors;
+    
+   return jac, ind
 end
 
 # Define a struct that holds data we need in the local SNES routines below   
@@ -85,48 +117,50 @@ mutable struct Data
     dt
     dz
     De
+    jac
+    colors
 end
-user_ctx = Data(nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing);  # holds data we need in the local 
+user_ctx = Data(nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing);  # holds data we need in the local 
 
 
 function ComputeLocalResidual(dm, ArrayLocal_x, ArrayLocal_f, user_ctx)
     # Compute the local residual. The vectors include ghost points 
-    n           =   2.0
-    m           =   3.0
+    n           =   3.0
+    m           =   2.0
     dt          =   user_ctx.dt;
     dz          =   user_ctx.dz;
     De          =   user_ctx.De;
 
     Phi         =   PETSc.DMStagGetGhostArrayLocationSlot(dm,ArrayLocal_x   ,   PETSc.DMSTAG_ELEMENT,   0); 
     Phi_old     =   PETSc.DMStagGetGhostArrayLocationSlot(dm,user_ctx.xold_l,   PETSc.DMSTAG_ELEMENT,   0); 
-    Pe          =   PETSc.DMStagGetGhostArrayLocationSlot(dm,ArrayLocal_x   ,   PETSc.DMSTAG_LEFT,      0); 
-    Pe_old      =   PETSc.DMStagGetGhostArrayLocationSlot(dm,user_ctx.xold_l,   PETSc.DMSTAG_LEFT,      0); 
+    Pe          =   PETSc.DMStagGetGhostArrayLocationSlot(dm,ArrayLocal_x   ,   PETSc.DMSTAG_ELEMENT,   1); 
+    Pe_old      =   PETSc.DMStagGetGhostArrayLocationSlot(dm,user_ctx.xold_l,   PETSc.DMSTAG_ELEMENT,   1); 
     
     res_Phi     =   PETSc.DMStagGetGhostArrayLocationSlot(dm,ArrayLocal_f,      PETSc.DMSTAG_ELEMENT,   0); 
-    res_Pe      =   PETSc.DMStagGetGhostArrayLocationSlot(dm,ArrayLocal_f,      PETSc.DMSTAG_LEFT,      0); 
+    res_Pe      =   PETSc.DMStagGetGhostArrayLocationSlot(dm,ArrayLocal_f,      PETSc.DMSTAG_ELEMENT,   1); 
     
     # compute the FD stencil
-    nPhi        =   length(Phi)-1;    # array length
+    sx, sn          =     PETSc.DMStagGetCentralNodes(dm);          # indices of (center/element) points, not including ghost values.
     
     # Porosity residual @ center points
-    i               =   2:nPhi-1
-    res_Phi[1]      =   Phi[1]   -1.0;                                  # left BC
-    res_Phi[nPhi]   =   Phi[nPhi]-1.0;                                  # right BC
-    res_Phi[i]      =   (Phi[i] - Phi_old[i])/dt +   ((Phi[i .+ 0].^n) .* ( (Pe[i .+ 1] - Pe[i     ])/dz .+ 1.0)  
-                                                    - (Phi[i .- 1].^n) .* ( (Pe[i     ] - Pe[i .- 1])/dz .+ 1.0))/dz
+    iz                  =   sx[1]:sn[1];            # Phi is on center points
+    i                   =   iz[2:end-1];
+    res_Phi[iz[1]]      =   Phi[iz[1]]   - 1.0;                                  # left BC
+    res_Phi[iz[end]]    =   Phi[iz[end]] - 1.0;                                  # right BC
+    res_Phi[i]          =   (Phi[i] - Phi_old[i])/dt + De.*(Pe[i]-Pe_old[i])/dt + (Phi[i].^m)   .* Pe[i]
    
     # Pressure update @ nodal points
-    nP              =    length(Pe);    
-    i               =    2:nP-1
-    res_Pe[1]       =    Pe[1]  - 0.;                                   # left BC
-    res_Pe[nP]      =    Pe[nP] - 0.;                                   # right BC
-    res_Pe[i]       =    De.*(Pe[i]-Pe_old[i])/dt -   ((Phi[i .+ 0].^n) .* ( (Pe[i .+ 1] - Pe[i     ])/dz .+ 1.0)  
-                                                     - (Phi[i .- 1].^n) .* ( (Pe[i     ] - Pe[i .- 1])/dz .+ 1.0))/dz +    (Phi[i].^m)   .* Pe[i];
+    iz                  =   sx[1]:sn[1];          # Pe is on center points as well (dof=2)
+    i                   =   iz[2:end-1];
+    res_Pe[iz[1]]       =   Pe[iz[1]]   - 0.;                                   # left BC
+    res_Pe[iz[end]]     =   Pe[iz[end]] - 0.;                                   # right BC
+    res_Pe[i]           =   De.*(Pe[i]-Pe_old[i])/dt -   (   ((0.5*(Phi[i .+ 1] + Phi[i .+ 0])).^n) .* ( (Pe[i .+ 1] - Pe[i     ])/dz .+ 1.0)  
+                                                          -  ((0.5*(Phi[i .- 1] + Phi[i .+ 0])).^n) .* ( (Pe[i     ] - Pe[i .- 1])/dz .+ 1.0))/dz +    (Phi[i].^m)   .* Pe[i];
 
     # Cleanup
     Base.finalize(Phi);    Base.finalize(Phi_old);       
     Base.finalize(Pe);     Base.finalize(Pe_old);       
-                                           
+
 end
 
 
@@ -138,38 +172,37 @@ function SetInitialPerturbations(user_ctx, x_g)
     vec_coord   =   PETSc.DMGetCoordinatesLocal(user_ctx.dm);
     Coord       =   PETSc.DMStagVecGetArray(DMcoord, vec_coord);
     Z_cen       =   PETSc.DMStagGetGhostArrayLocationSlot(user_ctx.dm,Coord, PETSc.DMSTAG_ELEMENT,    0); # center (has 1 extra)
-    Z           =   PETSc.DMStagGetGhostArrayLocationSlot(user_ctx.dm,Coord, PETSc.DMSTAG_LEFT,       0)
-    user_ctx.dz =   Z[2]-Z[1];
+    user_ctx.dz =   Z_cen[2]-Z_cen[1];
 
     Phi         =   PETSc.DMStagGetGhostArrayLocationSlot(user_ctx.dm,user_ctx.x_l,     PETSc.DMSTAG_ELEMENT, 0); 
-    Pe          =   PETSc.DMStagGetGhostArrayLocationSlot(user_ctx.dm,user_ctx.x_l,     PETSc.DMSTAG_LEFT,    0); 
+    Pe          =   PETSc.DMStagGetGhostArrayLocationSlot(user_ctx.dm,user_ctx.x_l,     PETSc.DMSTAG_ELEMENT, 1); 
     Phi_old     =   PETSc.DMStagGetGhostArrayLocationSlot(user_ctx.dm,user_ctx.xold_l,  PETSc.DMSTAG_ELEMENT, 0); 
-    Pe_old      =   PETSc.DMStagGetGhostArrayLocationSlot(user_ctx.dm,user_ctx.xold_l,  PETSc.DMSTAG_LEFT,    0); 
+    Pe_old      =   PETSc.DMStagGetGhostArrayLocationSlot(user_ctx.dm,user_ctx.xold_l,  PETSc.DMSTAG_ELEMENT, 1); 
     
-    Phi0 =1; dPhi1=8; dPhi2=1; z1=10; z2=40; lambda=1
+    Phi0 =1.0; dPhi1=8.0; dPhi2=1.0; z1=0.0; z2=40.0; lambda=1.0
     dPe1        =   dPhi1/user_ctx.De; 
     dPe2        =   dPhi2/user_ctx.De;
     
-    Phi        .=   Phi0 .+ dPhi1.*exp.( -(Z .- z1).^2.0) +  dPhi2.*exp.( -(Z .- z2).^2.0);
-    Pe         .=          -dPe1 .*exp.( -(Z .- z1).^2.0) -   dPe2.*exp.( -(Z .- z2).^2.0);
+    Phi        .=   Phi0 .+ dPhi1.*exp.( -((Z_cen .- z1).^2.0)/lambda^2) +  dPhi2.*exp.( -((Z_cen .- z2).^2.0)/lambda^2);
+    Pe         .=          -dPe1 .*exp.( -((Z_cen .- z1).^2.0)/lambda^2) -   dPe2.*exp.( -((Z_cen .- z2).^2.0)/lambda^2);
 
-    Phi_old    .=   Phi0 .+ dPhi1.*exp.( -(Z .- z1).^2.0) +  dPhi2.*exp.( -(Z .- z2).^2.0);
-    Pe_old     .=          -dPe1 .*exp.( -(Z .- z1).^2.0) -   dPe2.*exp.( -(Z .- z2).^2.0);
+    Phi_old    .=   Phi0 .+ dPhi1.*exp.( -((Z_cen .- z1).^2.0)/lambda^2) +  dPhi2.*exp.( -((Z_cen .- z2).^2.0)/lambda^2);
+    Pe_old     .=          -dPe1 .*exp.( -((Z_cen .- z1).^2.0)/lambda^2) -   dPe2.*exp.( -((Z_cen .- z2).^2.0)/lambda^2);
 
     # Copy local into global residual vector
     PETSc.DMLocalToGlobal(user_ctx.dm,user_ctx.x_l, PETSc.INSERT_VALUES, x_g) 
 
     # send back coordinates
-    return Z, Z_cen
+    return Z_cen
 end
 
 
 # Main Solver
-nx              =   301;
+nx              =   2001;
 L               =   150;
-user_ctx.De     =   1e1;        # Deborah number
-user_ctx.dt     =   2e-2;       # Note that the timestep has to be tuned a bit depending on the     
-user_ctx.dm     =   PETSc.DMStagCreate1d(MPI.COMM_SELF,PETSc.DM_BOUNDARY_NONE,nx,1,1);  # Pe-vertexes; Phi (melt fraction)=center
+user_ctx.De     =   1e-2;        # Deborah number
+user_ctx.dt     =   1e-3;        # Note that the timestep has to be tuned a bit depending on De in order to obtain convergence     
+user_ctx.dm     =   PETSc.DMStagCreate1d(MPI.COMM_SELF,PETSc.DM_BOUNDARY_NONE,nx,0,2);  # both Phi and Pe are on center points 
 PETSc.DMStagSetUniformCoordinatesExplicit(user_ctx.dm, -20, L)            # set coordinates
 x_g             =   PETSc.DMCreateGlobalVector(user_ctx.dm)
 
@@ -183,10 +216,10 @@ J               =   PETSc.DMCreateMatrix(user_ctx.dm);                  # Jacobi
 
 
 # initial non-zero structure of jacobian
-Z, Z_cen        =   SetInitialPerturbations(user_ctx, x_g)
+Z_cen           =   SetInitialPerturbations(user_ctx, x_g)
 
 x0              =   PETSc.VecSeq(rand(size(x_g,1)));
-J_julia         =   FormJacobian!(x0.ptr, J, J, user_ctx)
+J_julia,ind     =   FormJacobian!(x0.ptr, J, J, user_ctx)
 
 
 S = PETSc.SNES{Float64}(MPI.COMM_SELF, 0; 
@@ -229,13 +262,15 @@ while time<25.0
     time += user_ctx.dt;
     it   += 1;
 
-    if mod(it,20)==0  # Visualisation
+    if mod(it,200)==0  # Visualisation
         # Extract values and plot 
         Phi         =   PETSc.DMStagGetGhostArrayLocationSlot(user_ctx.dm,user_ctx.x_l,     PETSc.DMSTAG_ELEMENT, 0); 
-        Pe          =   PETSc.DMStagGetGhostArrayLocationSlot(user_ctx.dm,user_ctx.x_l,     PETSc.DMSTAG_LEFT,    0); 
+        Pe          =   PETSc.DMStagGetGhostArrayLocationSlot(user_ctx.dm,user_ctx.x_l,     PETSc.DMSTAG_ELEMENT, 1); 
 
-        p1 = plot(Phi[1:end-1], Z_cen[1:end-1],  ylabel="Z", xlabel="ϕ",  xlims=( 0.0, 2.0), label=:none, title="De=$(user_ctx.De)"); 
-        p2 = plot(Pe,           Z    ,                       xlabel="Pe", xlims=(-0.1, 0.1), label=:none, title="$(round(time;sigdigits=3))"); 
+        p1 = plot(Phi[1:end-1], Z_cen[1:end-1],  ylabel="Z", xlabel="ϕ",  xlims=( 0.0, 6.0), label=:none, title="De=$(user_ctx.De)"); 
+        p2 = plot(Pe[1:end-1],  Z_cen[1:end-1]    ,                       xlabel="Pe", xlims=(-1.25, 1.25), label=:none, title="$(round(time;sigdigits=3))"); 
+       # p1 = plot(Phi[1:end-1], Z_cen[1:end-1],  ylabel="Z", xlabel="ϕ",   label=:none, title="De=$(user_ctx.De)"); 
+       # p2 = plot(Pe[1:end-1],  Z_cen[1:end-1]    ,                       xlabel="Pe",  label=:none, title="$(round(time;sigdigits=3))"); 
         
         plot(p1, p2, layout=(1,2)); frame(anim)
 

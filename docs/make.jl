fdb5c185806704d251176b83b13badc30cfb67cf
Jeremy E Kozdon <jekozdon@nps.edu>
2021-06-07 09:21:04 -0700

    Remove stale docs

--- docs/make.jl
+++
@@ -1,9 +0,0 @@
-using Documenter, PETSc
-
-makedocs(modules = [PETSc])
-
-deploydocs(
-  deps = Deps.pip("mkdocs", "python-markdown-math"),
-  repo = "github.com/JuliaParallel/PETSc.jl.git",
-  julia = "0.4",
-)

8f4b6e8a913785bbd10848a8be8b0cae98df8a8b
Boris Kaus <kaus@uni-mainz.de>
2021-07-08 09:09:46 +0200

    updated docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,5 @@
 using Documenter, PETSc
 
-<<<<<<< HEAD
-DocMeta.setdocmeta!(PETSc, :DocTestSetup, :(using PETSc); recursive=true)
-
-makedocs(;
-    modules=[PETSc],
-    repo="https://github.com/JuliaParallel/PETSc/{commit}{path}#{line}",
-    sitename="PETSc.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://github.com/JuliaParallel/PETSc.jl",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-        "PETSc" => Any[
-            "DMStag" =>  "dmstag.md",
-        ],
-        "List of functions"  => "listfunctions.md"
-=======
 makedocs(;
     modules=[PETSc],
     sitename="PETSc.jl",
@@ -30,9 +11,9 @@ makedocs(;
         "Getting Started" => "man/getting_started.md",
         "PETSc" => Any[
             "Mat" =>  "man/mat.md",
+            "DMStag" =>  "man/dmstag.md",
         ],
         "List of functions"  => "man/listfunctions.md"
->>>>>>> 30e66665e4f65f42c22d953b96ab1fb234d08fb8
     ],
 )
 

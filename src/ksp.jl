d3f5d21f2c5259a73fb5aa378fd71a676090b4cb
Jeremy E Kozdon <jekozdon@nps.edu>
2021-07-08 15:54:51 -0700

    Add a petsc docs link function

--- src/ksp.jl
+++ src/ksp.jl
@@ -145,7 +145,8 @@ Base.show(io::IO, ksp::KSP) = _show(io, ksp)
 
 Gets the current iteration number; if the `solve!` is complete, returns the number of iterations used.
 
-https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPGetIterationNumber.html
+# External Links
+$(_doc_external("KSP/KSPGetIterationNumber"))
 """
 iters
 
@@ -155,7 +156,8 @@ iters
 
 Gets the last (approximate preconditioned) residual norm that has been computed.
 
-https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPGetResidualNorm.html
+# External Links
+$(_doc_external("KSP/KSPGetResidualNorm"))
 """
 resnorm
 

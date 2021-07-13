45aef20436bb623ea82334f5390e9b6bf1af5afb
Jeremy E Kozdon <jekozdon@nps.edu>
2021-07-13 15:24:16 -0700

    Switch to using multiple libraries from PETSc_jll

--- src/startup.jl
+++ src/startup.jl
@@ -12,7 +12,18 @@ function getlibs()
 end
 const libs = @static if !haskey(ENV, "JULIA_PETSC_LIBRARY")
     using PETSc_jll
-    ((PETSc_jll.libpetsc,),)
+    (
+     (PETSc_jll.libpetsc_Float64_Real_Int64,),
+     (PETSc_jll.libpetsc_Float32_Real_Int64,),
+     (PETSc_jll.libpetsc_Float64_Complex_Int64,),
+     (PETSc_jll.libpetsc_Float32_Complex_Int64,),
+     # XXX: The following cannot be used until all types are based on PetscLib
+     #      instead of PetscScalar
+     # (PETSc_jll.libpetsc_Float64_Real_Int32,),
+     # (PETSc_jll.libpetsc_Float32_Real_Int32,),
+     # (PETSc_jll.libpetsc_Float64_Complex_Int32,),
+     # (PETSc_jll.libpetsc_Float32_Complex_Int32,),
+    )
 else
     getlibs()
 end

a514db69866671daf8619779a9d939b803643e5a
Jeremy E Kozdon <jekozdon@nps.edu>
2021-07-19 16:27:01 -0700

    Add back matshell

--- src/matshell.jl
+++ src/matshell.jl
@@ -1,60 +1,106 @@
 """
-    MatShell{T}(obj, m, n)
+    MatShell(
+        petsclib::PetscLib,
+        obj::OType,
+        comm::MPI.Comm,
+        local_rows,
+        local_cols,
+        global_rows = LibPETSc.PETSC_DECIDE,
+        global_cols = LibPETSc.PETSC_DECIDE,
+    )
 
-Create a `m×n` PETSc shell matrix object wrapping `obj`.
+Create a `global_rows X global_cols` PETSc shell matrix object wrapping `obj`
+with local size `local_rows X local_cols`.
 
-If `obj` is a `Function`, then the multiply action `obj(y,x)`; otherwise it calls `mul!(y, obj, x)`.
-This can be changed by defining `PETSc._mul!`.
+The `obj` will be registered as an `MATOP_MULT` function and if if `obj` is a
+`Function`, then the multiply action `obj(y,x)`; otherwise it calls `mul!(y,
+obj, x)`.
 
+if `comm == MPI.COMM_SELF` then the garbage connector can finalize the object,
+otherwise the user is responsible for calling [`destroy`](@ref).
+
+# External Links
+$(_doc_external("Mat/MatCreateShell"))
+$(_doc_external("Mat/MatShellSetOperation"))
+$(_doc_external("Mat/MATOP_MULT"))
 """
-mutable struct MatShell{T,A} <: AbstractMat{T}
+mutable struct MatShell{PetscLib, PetscScalar, OType} <:
+               AbstractMat{PetscLib, PetscScalar}
     ptr::CMat
-    obj::A
+    obj::OType
 end
 
+struct MatOp{PetscLib, PetscInt, Op} end
 
-struct MatOp{T,Op} end
+function (::MatOp{PetscLib, PetscInt, LibPETSc.MATOP_MULT})(
+    M::CMat,
+    cx::CVec,
+    cy::CVec,
+)::PetscInt where {PetscLib, PetscInt}
+    r_ctx = Ref{Ptr{Cvoid}}()
+    LibPETSc.MatShellGetContext(PetscLib, M, r_ctx)
+    ptr = r_ctx[]
+    mat = unsafe_pointer_to_objref(ptr)
 
+    PetscScalar = getlib(PetscLib).PetscScalar
+    x = unsafe_localarray(WrapVec{PetscLib, PetscScalar}(cx); write = false)
+    y = unsafe_localarray(WrapVec{PetscLib, PetscScalar}(cy); read = false)
 
-function _mul!(y,mat::MatShell{T,F},x) where {T, F<:Function}
+    _mul!(y, mat, x)
+
+    Base.finalize(y)
+    Base.finalize(x)
+    return PetscInt(0)
+end
+
+function _mul!(
+    y,
+    mat::MatShell{PetscLib, PetscScalar, F},
+    x,
+) where {PetscLib, PetscScalar, F <: Function}
     mat.obj(y, x)
 end
 
-function _mul!(y,mat::MatShell{T},x) where {T}
+function _mul!(y, mat::MatShell, x) where {T}
     LinearAlgebra.mul!(y, mat.obj, x)
 end
 
-MatShell{T}(obj, m, n) where {T} = MatShell{T}(obj, MPI.COMM_SELF, m, n, m, n)
+# We have to use the macro here because of the @cfunction
+LibPETSc.@for_petsc function MatShell(
+    petsclib::$PetscLib,
+    obj::OType,
+    comm::MPI.Comm,
+    local_rows,
+    local_cols,
+    global_rows = LibPETSc.PETSC_DECIDE,
+    global_cols = LibPETSc.PETSC_DECIDE,
+) where {OType}
+    mat = MatShell{$PetscLib, $PetscScalar, OType}(C_NULL, obj)
 
+    # we use the MatShell object itself
+    ctx = pointer_from_objref(mat)
 
-@for_libpetsc begin
-    function MatShell{$PetscScalar}(obj::A, comm::MPI.Comm, m, n, M, N) where {A}
-        mat = MatShell{$PetscScalar,A}(C_NULL, obj)
-        # we use the MatShell object itsel
-        ctx = pointer_from_objref(mat)
-        @chk ccall((:MatCreateShell, $libpetsc), PetscErrorCode,
-          (MPI.MPI_Comm,$PetscInt,$PetscInt,$PetscInt,$PetscInt,Ptr{Cvoid},Ptr{CMat}),
-          comm, m, n, M, N, ctx, mat)
+    LibPETSc.MatCreateShell(
+        petsclib,
+        comm,
+        local_rows,
+        local_cols,
+        global_rows,
+        global_cols,
+        pointer_from_objref(mat),
+        mat,
+    )
 
-        mulptr = @cfunction(MatOp{$PetscScalar, MATOP_MULT}(), $PetscInt, (CMat, CVec, CVec))
-        @chk ccall((:MatShellSetOperation, $libpetsc), PetscErrorCode, (CMat, MatOperation, Ptr{Cvoid}), mat, MATOP_MULT, mulptr)
-        return mat
+    mulptr = @cfunction(
+        MatOp{$PetscLib, $PetscInt, LibPETSc.MATOP_MULT}(),
+        $PetscInt,
+        (CMat, CVec, CVec)
+    )
+    LibPETSc.MatShellSetOperation(petsclib, mat, LibPETSc.MATOP_MULT, mulptr)
+
+    if comm == MPI.COMM_SELF
+        finalizer(destroy, mat)
     end
 
-    function (::MatOp{$PetscScalar, MATOP_MULT})(M::CMat,cx::CVec,cy::CVec)::$PetscInt
-        r_ctx = Ref{Ptr{Cvoid}}()
-        @chk ccall((:MatShellGetContext, $libpetsc), PetscErrorCode, (CMat, Ptr{Ptr{Cvoid}}), M, r_ctx)
-        ptr = r_ctx[]
-        mat = unsafe_pointer_to_objref(ptr)
-
-        x = unsafe_localarray($PetscScalar, cx; write=false)
-        y = unsafe_localarray($PetscScalar, cy; read=false)
-
-        _mul!(y,mat,x)
-
-        Base.finalize(y)
-        Base.finalize(x)
-        return $PetscInt(0)
-    end
-
+    return mat
 end
